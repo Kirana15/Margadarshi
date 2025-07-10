616f9b2d6fbfa9703767a0835dacbd22dde00256
Kirana BV <116853759+Kirana15@users.noreply.github.com>
2025-07-10 13:38:16 +0530

    route_app.py

--- python/route_app.py
+++ python/route_app.py
@@ -39,8 +39,8 @@ if st.button("Plan Optimal Route"):
             st.success(" Optimized Visiting Order:")
             short_names = [place.split(",")[0].strip() for place in tsp_route]
             st.markdown(" → ".join(short_names))
-            with st.expander("Disclaimer"):
-                 st.warning(
+            
+            st.warning(
                     "Disclaimer: The map may not represent official borders or jurisdictions. "
                     "No geopolitical assumptions are made by this application."
                 )
